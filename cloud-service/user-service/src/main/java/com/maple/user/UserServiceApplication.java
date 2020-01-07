b7dea7b15574e1e20648d418186ff926e41d94e0
hack-feng <1150640979@qq.com>
2020-01-07 18:06:02 +0800

    创建maven项目

---
+++ cloud-service/user-service/src/main/java/com/maple/user/UserServiceApplication.java
@@ -0,0 +1,24 @@
+package com.maple.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Hack-Feng
+ */
+@RestController
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+        System.out.println("项目启动成功了...");
+    }
+
+    @GetMapping("/test")
+    public String test(){
+        return "欢迎进入到SpringCloud江湖！";
+    }
+}
---
+++ cloud-service/user-service/src/test/java/com/maple/user/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maple.user;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
