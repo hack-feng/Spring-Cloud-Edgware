8c65181eef0b87c035677b02b1bb31bccc1b3408
hack-feng <1150640979@qq.com>
2020-01-11 15:55:17 +0800

    创建pub项目

---
+++ cloud-service/pub-service/src/main/java/com/maple/pub/PubServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maple.pub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PubServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PubServiceApplication.class, args);
+    }
+
+}
