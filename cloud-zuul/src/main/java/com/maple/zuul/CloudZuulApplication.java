f8dd18fd9f5e52af52a4548926efb57293d52a5d
hack-feng <1150640979@qq.com>
2020-02-02 10:49:04 +0800

    修改restTemplate超时配置

---
+++ cloud-zuul/src/main/java/com/maple/zuul/CloudZuulApplication.java
@@ -0,0 +1,13 @@
+package com.maple.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudZuulApplication.class, args);
+    }
+
+}
